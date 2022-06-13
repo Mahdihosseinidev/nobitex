dcef7cb807b1e6f8908889f8d87a549f36d702dc
meysam <meisam.design.ph@gmail.com>
2022-06-13 22:44:39 +0430

    style update: responsive footer

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,8 +1,8 @@
-let acc = document.getElementsByClassName("main-footer__nav-title");
+let accordion = document.getElementsByClassName("main-footer__nav-title");
 let index;
 
-for (index = 0; index < acc.length; index++) {
-  acc[index].addEventListener("click", function () {
+for (index = 0; index < accordion.length; index++) {
+  accordion[index].addEventListener("click", function () {
     const panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
